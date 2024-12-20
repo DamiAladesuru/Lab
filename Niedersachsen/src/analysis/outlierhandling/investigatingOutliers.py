167a2173b0b2d498b063138d2a2a2bc3fda38ce1
DamiAladesuru <damilolaaladesuru@gmail.com>
2024-12-20 14:10:46 +0100

    grid outlier detection worksheet

--- Niedersachsen/src/analysis/outlierhandling/investigatingOutliers.py
+++ Niedersachsen/src/analysis/outlierhandling/investigatingOutliers.py
@@ -3,16 +3,15 @@
 import os
 import seaborn as sns
 import joypy
-from joypy import joyplot
 from matplotlib import cm
 import matplotlib.pyplot as plt
 
 # %%
 os.chdir("C:/Users/aladesuru/Documents/DataAnalysis/Lab/Niedersachsen")
 
-###########################
-# Subsettings
-###########################
+########################################
+# General subsetting and filtering codes
+########################################
 # %% or subsetting
 gridclean = gridgdf_raw[~(gridgdf_raw['LANDKREIS'] == 'Küstenmeer Region Weser-Ems')| (gridgdf_raw['LANDKREIS'] == 'Lüneburg')]
 
@@ -26,8 +25,9 @@ print(edgegrids[['CELLCODE', 'LANDKREIS']])
 # %% 
 Niedergld = gld[gld['FLIK'].str.startswith('DENI')]
 
+
 ###########################
-# Transform to log and obtain z scrore
+# Transform field data to log and obtain z scrore
 ###########################
 # %%
 # Reset index and rename for consistency
@@ -52,15 +52,38 @@ for column in log_columns:
     gld_[f'{column}_zscore'] = gld_.groupby('year')[column].transform(lambda x: (x - x.mean()) / x.std())
 
 #%% Identify outliers (Z-score > 3 or Z-score < -3)
-outliers_par = gld_[gld_['par_log_zscore'] > 3]
+#outliers_par = gld_[gld_['par_log_zscore'] > 3]
 outliears_area = gld_[gld_['area_m2_log_zscore'] < -3]
-outliears_area = outliears_area.drop(columns='geometry')
+#outliears_area = outliears_area.drop(columns='geometry')
+gld_noout = gld_[~(gld_['area_m2_log_zscore'] < -3)]
 
+###########################
+# calculate z score at grid level
+###########################
+# %% Reset index and rename for consistency
+gridgdfout_ = gridgdfout.reset_index().rename(columns={'index': 'id'})
+
+# Select numerical columns except 'year' and 'kulturcode'
+data = gridgdfout_.select_dtypes(include=[np.number]).drop(['year'], axis=1)
+
+# Group by 'year' and calculate Z-scores within each group
+for column in data:
+    # Compute Z-score within each year group
+    gridgdfout_[f'{column}_zscore'] = gridgdfout_.groupby('year')[column].transform(lambda x: (x - x.mean()) / x.std())
+
+# %% drop grids with z score less than -1.55
+gridgdfout__ = gridgdfout_[~(gridgdfout_['fields_zscore'] < -1.55)]
+''' go to datamani sheet to create new grid_yearlyout for plotting aggregate line plot'''
+# see grids left out    
+zscoredout = gridgdfout_[(gridgdfout_['fields_zscore'] < -1.55)]
+zscoredoutdf = zscoredout.drop(columns='geometry')
+# %%
 ###########################
 # get polygons within a buffer
 ###########################
 '''you can use gridgdf or land shp as polygons_gdf'''
-# Combine all boundary geometries into a single geometry
+# %% Combine all boundary geometries into a single geometry
+polygons_gdf = gridgdf
 boundary_geometry = polygons_gdf.unary_union
 
 # Create a 10km inner buffer (negative buffer shrinks the boundary)
@@ -73,15 +96,22 @@ if not inner_buffer_10km.is_valid:
 # Select polygons that are within the inner buffer
 polygons_within_10km_inside = polygons_gdf[polygons_gdf.geometry.intersects(inner_buffer_10km)]
 
-#For grids in the upper left that are not included
-add = gridgdfno100[gridgdfno100['CELLCODE'].isin(['10kmE412N339','10kmE412N338','10kmE412N337','10kmE412N336'])]
+# %% see grids left out
+bufferedout = gridgdf[~(gridgdf['CELLCODE'].isin(polygons_within_10km_inside['CELLCODE']))]
+bufferedoutdf = bufferedout.drop(columns='geometry')
+# %%For grids in the upper left that are not included
+import pandas as pd
+add = gridgdf[gridgdf['CELLCODE'].isin(['10kmE412N339','10kmE412N338','10kmE412N337','10kmE412N336'])]
 #For grid in the middle left that ought not to be
 polygons_within_10km_inside = polygons_within_10km_inside[~(polygons_within_10km_inside['CELLCODE'] == '10kmE409N327')]
-
+polygons_within_10km_inside = pd.concat([polygons_within_10km_inside, add], ignore_index=True)
 
 ###########################
-# multi layer plot
+# %% multi layer plot
 ###########################
+boundary_gdf = gpd.read_file("N:/ds/data/Niedersachsen/verwaltungseinheiten/NDS_Landesflaeche.shp")
+boundary_gdf = boundary_gdf.to_crs("EPSG:25832")
+        
 base = polygons_gdf.plot(color='white', edgecolor='black', figsize=(10, 10))
 
 boundary_gdf.plot(ax=base, color='blue', alpha=0.5)
@@ -91,6 +121,11 @@ gpd.GeoSeries(inner_buffer_10km).plot(ax=base, color='red', alpha=0.2)
 polygons_within_10km_inside.plot(ax=base, color='green', alpha=0.7)
 plt.show()
 
+# %%
+# %%
+g =gridgdf[(gridgdf['year'] == 2016)]
+g.plot(color='white', edgecolor='black', figsize=(10, 10))
+plt.show()
 ###########################
 #scatter plots
 ###########################
@@ -100,7 +135,8 @@ stack_plots_in_grid(gld_, unique_years, scatterplot_par_area, "area_ha_log", "pa
 
 # %% scatter gridgdf
 #d = gridgdfnoout_[~(gridgdfnoout_['fields_zscore'] < -1.55)]
-stack_plots_in_grid(polygons_within_10km_inside, unique_years, scatterplot_mpar_marea, "mfs_ha", "medpar", ncols=4, figsize=(25, 15))
+unique_years = sorted(gridgdf['year'].unique())
+stack_plots_in_grid(gridgdf_, unique_years, scatterplot_mpar_marea, "fields", "medpar", ncols=4, figsize=(25, 15))
 
 
 ###########################
@@ -115,7 +151,7 @@ def box_plot(df, column):
 box_plot(gld, 'area_ha')
 
 # %% boxplot for every year
-data = gridgdfnoout
+data = gridgdf
 plt.figure(figsize=(12, 6))
 ax = sns.boxplot(data=data, x='year', y='medpar')
 
@@ -216,7 +252,14 @@ def hist_all_years(df, value_column):
     plt.show()
     
 # %%
-hist_all_years(pol, 'fields')
+hist_all_years(gridgdfout__, 'medfs_ha')
+
+###########################
+# geoplot
+###########################
+# %%
+geoData = polygons_within_10km_inside.to_crs(epsg=4326)
+plot_facet_choropleth_with_geoplot(geoData, column='medfs_ha_percdiff_to_y1', cmap='plasma', year_col='year', ncols=4)
 
 ###########################
 # plot fields and grids
@@ -336,3 +379,96 @@ def plot_gridcell_single(gridgdf, gridcell, year):
 
 # Example usage
 plot_gridcell_single(gridgdf_raw, '10kmE416N330', 2017)
+
+################################
+# grid outliers removal final
+'''criteria: fields < 300'''
+################################
+# %%
+outliers300 =gridgdf[(gridgdf['fields'] < 300)]
+outliers300_ =outliers300.drop(columns='geometry')
+
+# %%
+gridgdf_ = gridgdf[~(gridgdf['fields'] < 300)]
+
+# %% examine distribution of medpar after dropping grids with fields < 300
+data = gridgdf_
+plt.figure(figsize=(12, 6))
+ax = sns.boxplot(data=data, x='year', y='medpar')
+
+# %%
+stack_plots_in_grid(gridgdf_, unique_years, scatterplot_mpar_marea, "fields", "medpar", ncols=4, figsize=(25, 15))
+
+
+####################################################################
+# verifying grids dropped for having fields < 300
+####################################################################
+'''1. see if each unique grid was dropped in all 12 years'''
+# %% Get unique CELLCODE values and their count of occurrences
+celcode_counts = outliers300_['CELLCODE'].value_counts()
+# Convert the result to a DataFrame for better readability
+celcode_counts_df = celcode_counts.reset_index()
+celcode_counts_df.columns = ['CELLCODE', 'count']
+''' 2. next goal is that: for some of the dropped grids,
+there are some which have less than 12 years.
+we need to check if the fact that these grids have less than 300 fields 
+makes them outliers and if so, we should probably make sure that other years not
+already dropped are also dropped. If not, we should add them back to the data.'''
+# first step is to verify in gridgdf what the values of fields are for these grid
+#in all years. This allows us to know why these grids were not identified as
+# containing < 300 fields for 12 years
+# before testing, filter gridgdf for a CELLCODE, remove geometry and check
+filtered_df = gridgdf[gridgdf['CELLCODE'] == '10kmE417N340']
+filtered_df_ = filtered_df.drop(columns='geometry')
+
+# %% then filter out all rows of each target grid from the data
+filtered = gridgdf[gridgdf['CELLCODE'] == '10kmE427N319']
+# this is because we will add all back to see the effect of the grid
+# %% aalso drop thm from cleaned data so that we don't have them duplicated
+gir = gridgdf_[~(gridgdf_['CELLCODE'] == '10kmE427N319')]
+# gir should be equal to gridgdf_ - number of rows of this grid that 
+# was retained as having fields > 300
+# %% add filtered back to this version of gridgdf
+testgdf = pd.concat([gir, filtered], ignore_index=True)
+# check number of rows in testgdf.
+# it should be equal to gridgdf_ + number of rows that
+# was cleaned out for this grid
+# %% examine distribution of medpar after adding back filtered = 10kmE417N340
+data = testgdf
+plt.figure(figsize=(12, 6))
+ax = sns.boxplot(data=data, x='year', y='medpar')
+
+''' Learnings:
+T1: adding back '10kmE417N340' to 2016 does not change the distribution of medpar and fields
+we should add this back to the data
+
+'10kmE427N319': box and scatterplots look the same as gridgdf_. this grid is indifferent
+'10kmE430N325': box and scatterplots look the same as gridgdf_. this grid is indifferent
+'10kmE414N324': box and scatterplots look the same as gridgdf_. this grid is indifferent
+'10kmE442N331': we see a jump in medpar in 2018. this grid is an outlier
+'10kmE429N339': we see a outliers in medpar in 2021, 22, 23. this grid is an outlier
+'10kmE433N331': box and scatterplots look the same as gridgdf_. this grid is indifferent
+'10kmE438N336': we see lower limit outliers in medpar in years except 2015 and 2017. this grid is an outlier
+'''
+# %% if indifferent i.e., cellcode is ['10kmE417N340', '10kmE427N319', '10kmE430N325', '10kmE414N324', '10kmE433N331']
+# filter the cellcode from outliers300
+
+# List of CELLCODE values to filter
+cellcodes = ['10kmE417N340', '10kmE427N319', '10kmE430N325', '10kmE414N324', '10kmE433N331']
+# Filter the DataFrame for the specified CELLCODE values
+notoutlier = outliers300[outliers300['CELLCODE'].isin(cellcodes)]
+notoutlier_ = notoutlier.drop(columns='geometry')
+
+# %%
+outliers300 = outliers300[~(outliers300['CELLCODE'].isin(cellcodes))]
+# %% add notoutlier back to gridgdf_
+gridgdffin = pd.concat([gridgdf_, notoutlier], ignore_index=True)
+
+# %% examine distribution of medpar 
+data = gridgdffin
+plt.figure(figsize=(12, 6))
+ax = sns.boxplot(data=data, x='year', y='medpar')
+
+# %% examine scatter plot of medpar and fields
+stack_plots_in_grid(data, unique_years, scatterplot_mpar_marea, "fields", "medpar", ncols=4, figsize=(25, 15))
+# plot should look like plot for gridgdf_
